6ee8574dfa85142a404315c31dbb3875743be120
TTMMJJ <2307234066@qq.com>
2017-11-16 20:13:50 +0800

    视频播放

--- ijkplay/src/main/java/com/bwie/ijkplay/MainActivity.java
+++ ijkplay/src/main/java/com/bwie/ijkplay/MainActivity.java
@@ -3,6 +3,7 @@ package com.bwie.ijkplay;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bwie.ijkplay.widget.media.IRenderView;
 import com.bwie.ijkplay.widget.media.IjkVideoView;
@@ -16,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         videoView = (IjkVideoView) findViewById(R.id.video_view);
         videoView.setAspectRatio(IRenderView.AR_ASPECT_FIT_PARENT);
         videoView.setVideoURI(Uri.parse("http://mp4.vjshi.com/2013-05-28/2013052815051372.mp4"));
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         videoView.start();
     }
 }
